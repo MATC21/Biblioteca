bf008273fb356de9cb7b44e38774b4a01356a3b4
MATC21 <ma.ona@itq.edu.ec>
2023-12-13 11:31:51 -0500

    Clase 2

---
+++ SistemBiblioteca/Controllers/AutorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemBiblioteca.Models.Entidades;
+
+namespace SistemBiblioteca.Controllers
+{
+    public class AutorController : Controller
+    {
+        public IActionResult Index()
+        {
+            var autor = new Autor();
+            autor.idAutor = 1;
+            autor.nombreAutor = "DASDAS";
+            autor.apellidoAutor = "HHFYH";
+
+            return View(autor);
+        }
+    }
+}
--- SistemBiblioteca/Models/Entidades/Autor.cs
+++ SistemBiblioteca/Models/Entidades/Autor.cs
@@ -3,7 +3,7 @@
     public class Autor
     {
         public int idAutor { get; set; }
-        public string nombreAutor { get; set; }
-        public string apellidoAutor { get; set; }
+        public string? nombreAutor { get; set; }
+        public string? apellidoAutor { get; set; }
     }
 }
