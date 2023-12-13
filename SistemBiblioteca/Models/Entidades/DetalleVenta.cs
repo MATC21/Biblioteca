54c4695d545f579e514a86410f1a5850e03d8cbc
MATC21 <ma.ona@itq.edu.ec>
2023-12-13 10:23:04 -0500

    Entidades

---
+++ SistemBiblioteca/Models/Entidades/DetalleVenta.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemBiblioteca.Models.Entidades {
+    public partial class DetalleVenta
+    {
+        public int IdDetalleVenta { get; set; }
+        public int? IdVenta { get; set; }
+        public int? IdLibro { get; set; }
+        public int? Cantidad { get; set; }
+
+        public virtual Libro? IdLibroNavigation { get; set; }
+        public virtual Ventas? IdVentaNavigation { get; set; }
+    }
+}
+
+
